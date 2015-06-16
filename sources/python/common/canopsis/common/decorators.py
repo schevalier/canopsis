b2f9d62c3c9d2f83e4924cf7af88d5cb9971efb4
b3j0f <jlabejof@yahoo.fr>
2015-06-16 17:55:02 +0200

    :add: add the decorator 'deprecated' in canopsis.common.decorators

---
+++ sources/python/common/canopsis/common/decorators.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+# --------------------------------
+# Copyright (c) 2015 "Capensis" [http://www.capensis.com]
+#
+# This file is part of Canopsis.
+#
+# Canopsis is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Canopsis is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Canopsis.  If not, see <http://www.gnu.org/licenses/>.
+# ---------------------------------
+
+"""Define a set of decorators useful to enrich the canopsis runtime.
+"""
+
+__all__ = ['deprecated']
+
+from functools import wraps
+
+from logging import Logger
+
+from inspect import ismethod
+
+
+def _findlogger(func, args, kwargs):
+    """Find a logger from a or func call arguments.
+
+    :param func: function from where find a logger.
+    :param tuple args: vargargs from where find a logger.
+    :param dict kwargs: kwargs from where find a logger.
+    :return: first logger found from (in the order):
+
+        - func.__this__.logger
+        - kwargs['logger']
+        - args.
+        - kwargs.
+    :rtype: Logger
+    """
+
+    # initialize the result
+    result = None
+    # find a logger from func instance if func is a method
+    if ismethod(func):
+        instance = getattr(func, '__self__', None)
+        if instance is not None:
+            result = getattr(instance, 'logger', None)
+
+    if result is None:
+        if 'logger' in kwargs:
+            logger = kwargs['logger']
+            if isinstance(logger, Logger):
+                result = logger
+        else:
+            for arg in args:
+                if isinstance(arg, Logger):
+                    result = arg
+                    break
+            else:
+                for key in kwargs:
+                    value = kwargs[key]
+                    if isinstance(value, Logger):
+                        result = value
+                        break
+
+    return result
+
+
+def deprecated(start, reason=None, logger=None):
+    """Depreciate a function in enriching its docstring and in logging a
+    message when the function is used.
+
+    :param str start: date or version of function deprecation.
+    :param str reason: additional message to add to the docstring if given.
+    :param Logger logger: logger to use if given.
+    :return: deprecated function.
+    """
+
+    def depreciate(func):
+        """Depreciate input function.
+
+        :param func: function to depreciate.
+        :return: wrapper from func which log messages.
+        """
+
+        logmessage = "{0} is deprecated from {1}.".format(func, start)
+        if reason is not None:
+            logmessage = "{0}\nReason: {1}".format(logmessage, reason)
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            """Log a deprecation message before running the function ``func``.
+
+            :param args: func call varargs.
+            :param kwargs: func call keywords.
+            """
+
+            log = logger
+
+            if log is None:
+                log = _findlogger(func, args, kwargs)
+
+            if log is None:  # raise an error if logger is None
+                raise RuntimeError(logmessage)
+            else:  # otherwise log a warning message
+                log.warning(logmessage)
+
+            # execute func with args and kwargs and return the result
+            return func(*args, **kwargs)
+
+        # update func docstring
+        docstring = "{0}\n{1}".format(logmessage, func.__doc__)
+        func.__doc__ = docstring
+
+        return wrapper
+
+    return depreciate
---
+++ sources/python/common/test/decorators.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# --------------------------------
+# Copyright (c) 2015 "Capensis" [http://www.capensis.com]
+#
+# This file is part of Canopsis.
+#
+# Canopsis is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Canopsis is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Canopsis.  If not, see <http://www.gnu.org/licenses/>.
+# ---------------------------------
+
+"""decorators unit tests.
+"""
+
+from unittest import main, TestCase
+from canopsis.common.decorators import deprecated
+
+
+class DeprecatedTest(TestCase):
+    """Test the decorator deprecated.
+    """
+
+    class LoggerMock(object):
+        """Logger Mock.
+        """
+
+        def __init__(self):
+
+            super(DeprecatedTest.LoggerMock, self).__init__()
+
+            self.msg = None
+
+        def warning(self, msg):
+            """log the warning message in setting to this the input msg.
+            :param str msg: message to set to this.
+            """
+
+            self.msg = msg
+
+    class ClassWithLogger(object):
+        """Class with logger.
+
+        This logger is its ``logger`` attribute.
+        """
+
+        def __init__(self):
+
+            super(DeprecatedTest.ClassWithLogger, self).__init__()
+
+            self.logger = DeprecatedTest.LoggerMock()
+
+        def test(self, *args, **kwargs):
+            """Empty test.
+            """
+
+    def setUp(self):
+
+        self.start = 'test'
+
+    def test_nologger(self):
+        """Test to depreciate a function without logger.
+        """
+
+        @deprecated(start=self.start)
+        def func():
+            """Test function
+            """
+
+        self.assertRaises(RuntimeError, func())
+
+    def test_loggermock(self):
+        """Test to depreciate a function with a logger.
+        """
+
+        logger = DeprecatedTest.LoggerMock()
+
+        @deprecated(start=self.start, logger=logger)
+        def func():
+            """Test function.
+            """
+
+        self.assertIsNone(logger.msg)
+
+        func()
+
+        self.assertIsNotNone(logger.msg)
+
+    def test_classwithlogger(self):
+        """Test to depreciate a method.
+        """
+
+        instance = DeprecatedTest.ClassWithLogger()
+
+        self.assertIsNone(instance.logger.msg)
+
+        instance.test()
+
+        self.assertIsNotNone(instance.logger.msg)
+
+    def test_args(self):
+        """Test to log a message from a logger in varargs.
+        """
+
+        logger = DeprecatedTest.LoggerMock()
+
+        @deprecated(start=self.start)
+        def func(*args):
+            """Test function.
+            """
+
+        self.assertIsNone(logger.msg)
+
+        func(self.start, logger, self.start)
+
+        self.assertIsNotNone(logger.msg)
+
+    def test_kwargs(self):
+        """Test to log a message from a logger in keywords.
+        """
+
+        logger = DeprecatedTest.LoggerMock()
+
+        @deprecated(start=self.start)
+        def func(**kwargs):
+            """Test function.
+            """
+
+        self.assertIsNone(logger.msg)
+
+        func(a=self.start, b=logger, c=self.start)
+
+        self.assertIsNotNone(logger.msg)
+
+    def test_order(self):
+        """Test to log messages related to order of appareance of a logger from
+        the instance, and arguments.
+        """
+
+        baselogger = DeprecatedTest.LoggerMock()
+        instance = DeprecatedTest.ClassWithLogger()
+        argslogger = DeprecatedTest.LoggerMock()
+        kwargslogger = DeprecatedTest.LoggerMock()
+
+        def initloggers():
+            """Init loggers.
+            """
+
+            baselogger.msg = None
+            instance.logger.msg = None
+            argslogger.msg = None
+            kwargslogger.msg = None
+
+        self.assertIsNone(baselogger.msg)
+        self.assertIsNone(instance.logger.msg)
+        self.assertIsNone(argslogger.msg)
+        self.assertIsNone(kwargslogger.msg)
+
+        @deprecated(start=self.start, logger=baselogger)
+        def func(*args, **kwargs):
+            """Test function.
+            """
+
+        func(argslogger, a=kwargslogger)
+
+        self.assertIsNone(baselogger.msg)
+        self.assertIsNone(instance.logger.msg)
+        self.assertIsNotNone(argslogger.msg)
+        self.assertIsNone(kwargslogger.msg)
+
+        initloggers()
+
+        func(argslogger, logger=kwargslogger)
+
+        self.assertIsNone(baselogger.msg)
+        self.assertIsNone(instance.logger.msg)
+        self.assertIsNone(argslogger.msg)
+        self.assertIsNotNone(kwargslogger.msg)
+
+        initloggers()
+
+        func()
+
+        self.assertIsNotNone(baselogger.msg)
+        self.assertIsNone(instance.logger.msg)
+        self.assertIsNone(argslogger.msg)
+        self.assertIsNotNone(kwargslogger.msg)
+
+        initloggers()
+
+        instance.test(argslogger, logger=kwargslogger)
+
+        self.assertIsNone(baselogger.msg)
+        self.assertIsNotNone(instance.logger.msg)
+        self.assertIsNone(argslogger.msg)
+        self.assertIsNotNone(kwargslogger.msg)
+
+if __name__ == '__main__':
+    main()
